a5252a2bb223b10fc3dc3ab33c5303a548c76ede
Alberto Marin <albertomr84@gmail.com>
2020-05-31 13:07:17 +0100

    add list of bbooks and authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -77,6 +77,18 @@ const RootQuery = new GraphQLObjectType({
         // get data from db or other source
         return _.find(authors, { id: args.id });
       }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
+      }
     }
   }
 });

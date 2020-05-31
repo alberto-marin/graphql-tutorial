4bb75e6cf1741088815a7723e574e3f9b0497b16
Alberto Marin <albertomr84@gmail.com>
2020-05-31 22:12:26 +0100

    setup mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+// connect to database
+mongoose.connect(
+  `mongodb+srv://alberto:${process.env.PASSWORD}@cluster0-kwbw2.mongodb.net/test?retryWrites=true&w=majority`
+);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use(
   '/graphql',

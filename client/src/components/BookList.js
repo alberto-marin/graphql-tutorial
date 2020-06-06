527c903abd17b26280dcf3b1b4b696be4576a2a2
Alberto Marin <albertomr84@gmail.com>
2020-06-06 10:44:33 +0100

    display data from mongo

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
-export default function BookList() {
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+function BookList(props) {
+  var data = props.data;
+  if (data.loading) {
+    return (
+      <div className='loading'>
+        <p>Loading books...</p>
+      </div>
+    );
+  }
+  if (data.error) {
+    return (
+      <div className='error'>
+        <p>Error: {data.error}</p>
+      </div>
+    );
+  }
   return (
     <div>
       <ul id='book-list'>
-        <li>Bookd name</li>
+        {data.books.map(book => {
+          return <li key={book.id}>{book.name}</li>;
+        })}
       </ul>
     </div>
   );
 }
+// bind this query to this component
+export default graphql(getBooksQuery)(BookList);
